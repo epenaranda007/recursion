45fac6434b4e958d2b0db9fb9d8e56e5d3fdad6f
Eugene Penaranda <eugenepenaranda007@gmail.com>
2017-01-28 02:54:22 -0800

    Completed getElementsByClassName and stringifyJSON.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var result = [];
+
+  searchForClassName(document.childNodes, 0);
+  
+  function searchForClassName(obj, index){
+  	if(index === obj.length){ /* base case */
+  		return;
+  	}
+  	if(obj[index].nodeType === 1){  /* 1 means element, filters only elements  */
+  		var cList = obj[index].classList;
+	  	if(cList.length !== 0 && constainsClass(cList, 0)){ 
+	  		result.push(obj[index]);
+	  	}		
+  		if(obj[index].childNodes.length > 0){
+  			searchForClassName(obj[index].childNodes, 0);
+  		}	  	
+	}
+  	searchForClassName(obj, index+1);
+  };
+
+  function constainsClass(array, index){
+  	if(array[index] === className){
+  		return true;
+  	}
+  	if(array.length-1 === index){
+  		return false;
+  	}
+  	return constainsClass(array, index+1);
+  }
+  
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj === 'function' || typeof obj === 'undefined'){
+  	return;
+  }
+  else if(obj === null){ 
+  	return "null";
+  }
+  else if(typeof obj === 'string'){
+  	return "\""+obj+"\"";
+  }
+  else if(Array.isArray(obj)){
+  	var result = "[";
+	if(obj.length === 0){ 
+  		return "[]";
+  	}
+  	result += obj.map(function(item){ return stringifyJSON(item);}).toString() + "]"; 
+  	return result;
+  }
+  else if(typeof obj === 'object'){ 
+  	var result = "{";
+	if(Object.keys(obj).length === 0){
+		return "{}";
+	}
+	_.each(obj, function(value, key){
+		if(stringifyJSON(value) !== undefined){
+			if(result.length === 1){ 
+				result += stringifyJSON(key) + ":" + stringifyJSON(value);
+			}
+			else{
+				result += "," + stringifyJSON(key) + ":" + stringifyJSON(value);
+			}	
+		}
+	});
+	return result + "}";
+  }
+  else{
+  	return obj.toString();
+  }
 };
+
